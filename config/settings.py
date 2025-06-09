6973ef456a9b80bd749a374bdbd0b7439386ecdb
hatakeyamareiya <r.hatakeyama.sys24@morijyobi.ac.jp>
2025-06-09 14:44:38 +0900

    インポートした２

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,9 @@ import environ
 from decouple import config
 from dj_database_url import parse as dburl
 
+print("全部インポートできました！")
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
